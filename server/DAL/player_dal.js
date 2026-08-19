23b1445a31e7c9d2894caea0319aea2f70933249
אביאל קדם <avielkedem2@gmail.com>
2026-08-19 12:15:59 +0300

    create function createPlayer and findPlayerById

---
+++ server/DAL/player_dal.js
@@ -0,0 +1,24 @@
+import { ObjectId } from "mongodb";
+import db from "../db/mongodb.js";
+
+
+const collection = await db.collection("player")
+
+
+async function createPlayer(body) {
+    const res = await collection.insertOne(body)
+    return {_id: res.insertedId, ...body}
+}
+
+
+async function findPlayerById(id) {
+    return await collection.findOne({_id: new ObjectId(id)})
+}
+
+
+
+
+export default {
+    createPlayer,
+    findPlayerById,
+} 
